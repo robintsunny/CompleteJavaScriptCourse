1edb3511fe9215c7f5737236598983e1fcb3fd37
robin <robintsunny@gmail.com>
2018-10-26 15:14:37 -0700

    How JS works: Hoisting, Scoping, and this

--- complete-javascript-course-master/3-how-JS-works/starter/script.js
+++ complete-javascript-course-master/3-how-JS-works/starter/script.js
@@ -1,17 +1,31 @@
+
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// calculateAge(1989);
+function calculateAge(year) {  //declaration
+    console.log(2018-year)
+};
 
 
+// retirement(29);  //expression, will not execute
+var retirement = function(age) {
+    console.log(65-age)
+}
 
 
+// variables
+console.log(age); //undefined
+var age = 29;
+console.log(age); // 29 
 
+function foo() {
+    var age = 30;
+    console.log(age)
+}
 
-
-
-
-
-
+foo(); // 30
+console.log(age) // 29
 
 
 
@@ -22,10 +36,8 @@
 
 
 // First scoping example
-
-/*
 var a = 'Hello!';
-first();
+first(); //  Hello!Hi!Hey!
 
 function first() {
     var b = 'Hi!';
@@ -36,15 +48,14 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
-first();
+first(); // Hello!Hi!Hey!
 
 function first() {
     var b = 'Hi!';
@@ -58,20 +69,57 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d); 
 }
-*/
+
+third(); // console a, but then error on b and c are not defined
+
+
+
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+console.log(this) //window is 'this' in the global context
+
+function calculateAge(year) {
+    console.log(2018-year);
+    console.log(this); //
+}
+
+calculateAge(1989); // this = window
 
 
 
+var john = {
+    name: 'John',
+    birthYear: 1989,
+    calculateAge: function () {
+        console.log('outer', this); // this = john object
+        console.log(2018-this.birthYear); 
+
+        function inner() {
+            console.log('inner', this); 
+            // this = window, inner() is a regular 
+            // function not attached to an object
+        }
+
+        inner();
+    }
+}
 
 
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
 
-
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge(); //this = mike

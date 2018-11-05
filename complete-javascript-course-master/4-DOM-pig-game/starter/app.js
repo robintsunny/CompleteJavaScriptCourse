79835fc471e9a60c2d722471192972844917a7b0
robin <robintsunny@gmail.com>
2018-11-05 10:25:13 -0600

    Update Score, Change Players, Handle Wins

--- complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -35,4 +35,47 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.style.display = 'block';
     diceDOM.src = `dice-${dice}.png`
 
+    // Handle 1's vs other Rolls
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector(`#current-${activePlayer}`).textContent= roundScore;
+    } else {
+        nextPlayer();
+    }
 })
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // add current score to global
+    scores[activePlayer] += roundScore
+
+    // update user interface
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+    
+    //check if player won game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+
+    } else {
+        //next player
+        nextPlayer();
+    }
+
+
+})
+
+function nextPlayer () {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+
+    // document.querySelector('.player-0-panel').classList.remove('active')
+    // document.querySelector('.player-1-panel').classList.add('active')
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+}
